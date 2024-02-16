786b8935b1cd5f8956209c803d390a40ed0fc7a6
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-02-16 00:17:23 -0800

    Summer Sale Shop added

---
+++ script.js
@@ -0,0 +1,57 @@
+
+let titleCount = 1;
+let totalPrice = 0;
+
+const cards = document.getElementsByClassName('card');
+for(let card of cards){
+    card.addEventListener('click', function(){
+
+        // get the title and price value 
+        const title = card.querySelector('h3').innerText;
+        const priceText = card.querySelector('span').innerText.split(' ')[1];
+        const price = parseFloat(priceText);
+
+        // appending title in the title container 
+        const titleContainer = document.getElementById('title-container');
+        const p = document.createElement('p');
+        p.innerText = titleCount + '. ' + title;
+        titleContainer.appendChild(p);
+        titleCount++
+
+        // calculate price 
+        totalPrice += price;
+        document.getElementById('totalPrice').innerText = totalPrice.toFixed(2);
+
+    })
+    
+}
+
+const applyBtn = document.getElementById('apply-btn');
+applyBtn.addEventListener('click', function(){
+    const couponElement = document.getElementById('input-field').value;
+    const coupon = couponElement.split(' ').join('').toUpperCase();
+    console.log(coupon)
+   if( totalPrice >= 200 ){
+        if( coupon === 'SELL200' ){
+            
+            // get 20% discount 
+            const discountElement = document.getElementById('discountPrice')
+            const discount = totalPrice * 20 / 100;
+            // set discount 
+            discountElement.innerText = discount.toFixed(2);
+
+            // get rest total price 
+            const restTotal = totalPrice - discount;
+            // set rest total price 
+            const total = document.getElementById('total');
+            total.innerText = restTotal.toFixed(2);
+        }
+        else{
+            alert('invalid coupon')
+        }
+
+   }else {
+    alert('buy more');
+   }
+     
+})
